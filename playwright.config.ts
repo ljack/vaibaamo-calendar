a727215bc860c064276f00082e93fc0c22ebce3d
Jarkko Lietolahti <Jarkko.lietolahti@gmail.com>
2025-12-30 03:17:57 +0200

    fix: set global locale to fi-FI for Playwright to fix all i18n mismatches

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    locale: 'fi-FI',
     extraHTTPHeaders: process.env.VERCEL_AUTOMATION_BYPASS_SECRET
       ? {
         'x-vercel-protection-bypass': process.env.VERCEL_AUTOMATION_BYPASS_SECRET,
